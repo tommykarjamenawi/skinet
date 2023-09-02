06a44baec6750274f5ba22f0f10cec155f28401f
Tommy Karjamenawi <54915266+tommykarjamenawi@users.noreply.github.com>
2023-09-03 00:03:46 +0200

    Finished Architecture

--- Core/Entities/Product.cs
+++ Core/Entities/Product.cs
@@ -1,8 +1,14 @@
 namespace Core.Entities
 {
-    public class Product
+    public class Product : BaseEntity
     {
-        public int Id { get; set; }
         public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; } // decimal is a better choice than double for money
+        public string PictureUrl { get; set; }
+        public ProductType ProductType { get; set; } // navigation property
+        public int ProductTypeId { get; set; } // foreign key
+        public ProductBrand ProductBrand { get; set; } // navigation property
+        public int ProductBrandId { get; set; } // foreign key
     }
 }
